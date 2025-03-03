12a23f4d235a84039947f8fa553980509259abd6
larsbuecker <lars.buecker1@gmail.com>
2025-03-03 20:56:00 +0100

    Fast bulk aktion implementation

--- frontend/app/components/transcriptionList/transcription-list-table.tsx
+++ frontend/app/components/transcriptionList/transcription-list-table.tsx
@@ -1,7 +1,7 @@
 import { use, useEffect, useState } from "react";
 import { DataTable } from "../ui/data-table";
 import { transcriptionListColumns, type TranscriptionListItem } from "./transcription-list-columns";
-import { getFileInfoById, getTranscriptionList } from "~/api";
+import { deleteTranscription, getFileById, getFileInfoById, getTranscriptionList } from "~/api";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
 import { Button } from "../ui/button";
 import { ChevronDown } from "lucide-react";
@@ -10,6 +10,7 @@ import { Card } from "../ui/card";
 
 const TranscriptionListTable = () => {
     const [data, setData] = useState<TranscriptionListItem[]>([]);
+    const [selectedRows, setSelectedRows] = useState<TranscriptionListItem[]>([]);
   
     useEffect(() => {
         async function fetchData() {
@@ -34,24 +35,63 @@ const TranscriptionListTable = () => {
         return () => clearInterval(intervalId); // Cleanup interval on component unmount
     }, []);
 
-    return (
+    const handleDeleteClicked = () => {
+        selectedRows.forEach(async (row) => {
+            await deleteTranscription(row.id);
+        });
+    }
 
+    const handleDownloadTranscriptionsClicked = () => {
+        selectedRows.forEach(async (row) => {
+            try {
+                const response = await getFileById(row.transcript_file_id);
+                const url = window.URL.createObjectURL(new Blob([response.data]));
+                const link = document.createElement("a");
+                link.href = url;
+                link.setAttribute("download", row.title + ".txt");
+                document.body.appendChild(link);
+                link.click();
+                link.remove();
+            } catch (error) {
+                console.error("Error downloading file:", error);
+            }
+        });
+    }
+
+    const handleDownloadAudiosClicked = () => {
+        selectedRows.forEach(async (row) => {
+            try {
+                const response = await getFileById(row.transcript_file_id);
+                const url = window.URL.createObjectURL(new Blob([response.data]));
+                const link = document.createElement("a");
+                link.href = url;
+                link.setAttribute("download", row.title);
+                document.body.appendChild(link);
+                link.click();
+                link.remove();
+            } catch (error) {
+                console.error("Error downloading file:", error);
+            }
+        });
+    }
+
+    return (
         <div className="container mx-auto my-10">
             <div className="w-full flex justify-end pb-4">
-                <DropdownMenu>
+                <DropdownMenu >
                     <DropdownMenuTrigger asChild>
-                        <Button variant="outline" className="ml-auto">Actions <ChevronDown/></Button>
+                        <Button disabled={selectedRows.length == 0} variant="outline" className="ml-auto">Actions <ChevronDown/></Button>
                     </DropdownMenuTrigger>
                     <DropdownMenuContent align="end">
-                        <DropdownMenuItem>Delete</DropdownMenuItem>
-                        <DropdownMenuItem>Download Transcriptions</DropdownMenuItem>
-                        <DropdownMenuItem>Download Audios</DropdownMenuItem>
+                        <DropdownMenuItem onClick={handleDeleteClicked}>Delete</DropdownMenuItem>
+                        <DropdownMenuItem onClick={handleDownloadTranscriptionsClicked}>Download Transcriptions</DropdownMenuItem>
+                        <DropdownMenuItem onClick={handleDownloadAudiosClicked}>Download Audios</DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
 
             </div>
             <div >
-                <DataTable columns={transcriptionListColumns} data={data} onSelectionChange={(value) => {console.log(value)}}/>
+                <DataTable columns={transcriptionListColumns} data={data} onSelectionChange={(value) => { setSelectedRows(value) }}/>
             </div>
         </div>
     )
